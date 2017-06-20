94419a2dafdf70547f91a04097b5c7c9894f2db3
Srijan Manandhar <srijan.manandhar@gmail.com>
2017-06-21 02:14:03 +0300

    Added few templates and style pages

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'webapp'
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^restaurant/$', views.index, name="index"),
+    url(r'^restaurant/(?P<restaurant_id>[0-9]+)/$', views.detail, name="detail"),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
+from .models import Restaurant
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Restaurant Hub, Join the Hub!!")
+    restaurant_list = Restaurant.objects.order_by('-created_at') [:5]
+    context = { 'restaurant_list': restaurant_list }
+    return render(request, 'webapp/index.html', context)
+
+def detail(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
+    return render(request, 'webapp/detail.html', {'restaurant':restaurant})
